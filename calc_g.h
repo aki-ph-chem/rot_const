200ecc41ec2bc08a8c31a273a52894665a13acdb
akki <saikurisuto@gmail.com>
2021-03-11 17:01:38 +0900

    重心クラス＿うまくいかん

---
+++ calc_g.h
@@ -0,0 +1,16 @@
+
+#ifndef _CALC_G_H_
+#define _CALC_G_H_
+
+class calc_g_coor{
+
+
+public:
+ 
+       double calc_g(double* coor,double* Mass);
+       double calc_g_sys(double* coor,double* g);
+
+};
+
+
+# endif //_CALC_G_H_
