821fc51725123578274d79ba1081d28ee1a64826
aki-ph-chem <77914655+aki-ph-chem@users.noreply.github.com>
2021-04-18 12:22:15 +0900

    dynam brach からmasterに戻る

--- R/coordinates_picker.R
+++ R/coordinates_picker.R
@@ -1,40 +1,41 @@
 
+#This tool is made for pick up coordinates from Gaussian .LOG file
+
+##load library###
+
 library(dplyr)
 library(rgl)
 library(stringr)
-setwd("c:/Users/simiz/Desktop/rot_const/R")
 
-###########データの取得#############
-#AN=26 #原子数
+## set working directry 
 
+my_working_directory = "c:/Users/simiz/Desktop/rot_const/R"
+
+setwd(my_working_directory)
+
+
+##########  information of input #######
 num_of_atoms = 21
+ 
+input_file_name = "DIBENZOTHIOPHENE_DFT_3-21G_B3LYP.LOG"
 
-#stb3=read.csv("stb3.csv")  
+#load .LOG file
 
-file_name = "DIBENZOTHIOPHENE_DFT_3-21G_B3LYP.LOG"
+Log_file = readLines(input_file_name)
 
-Log_file = readLines(file_name)
-
-#n=which(stb3[,]=="Standard orientation:") %>% max()
 
 flag_for_coordinates = Log_file[]== "                         Standard orientation:                         "
 
 num_of_line = which(flag_for_coordinates) %>% max()
 
-#atm2=stb3[(n+5):(n+5+AN-1),]
 
 coordinates = Log_file[(num_of_line + 5):(num_of_line + 5 + num_of_atoms -1)]
 
-
-#atm_n2=atm2[] %>% stringr::str_split(" ") %>% type.convert() ##　文字列を分割して数値に変換
 
 coordinates_numerical_crude = coordinates[] %>% stringr::str_split(" ") %>% type.convert() 
 
 coordinates_numerical_crude
 
-#  atm_n2 = atm2[] %>% stringr::str_split(" ") 
-
-# atm_n2 = atm_n2[grep("^[9-9]{3}$",atm_n2)]
 
 A=matrix(0,num_of_atoms,6)
 
@@ -49,5 +50,7 @@ for(i in 1:num_of_atoms){
     }
 }
 
+
 coordinates_numerical
+
 
