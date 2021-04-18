741e16cec177fa0b178e5ed624a5da73d1831fac
aki-ph-chem <77914655+aki-ph-chem@users.noreply.github.com>
2021-04-18 11:53:14 +0900

    座標取得ツールはok

--- R/coordinates_picker.R
+++ R/coordinates_picker.R
@@ -2,34 +2,52 @@
 library(dplyr)
 library(rgl)
 library(stringr)
-
 setwd("c:/Users/simiz/Desktop/rot_const/R")
 
 ###########データの取得#############
-AN=26 #原子数
+#AN=26 #原子数
 
-stb3=read.csv("stb3.csv")  
+num_of_atoms = 21
 
-n=which(stb3[,]=="Standard orientation:") %>% max()
+#stb3=read.csv("stb3.csv")  
+
+file_name = "DIBENZOTHIOPHENE_DFT_3-21G_B3LYP.LOG"
+
+Log_file = readLines(file_name)
+
+#n=which(stb3[,]=="Standard orientation:") %>% max()
+
+flag_for_coordinates = Log_file[]== "                         Standard orientation:                         "
+
+num_of_line = which(flag_for_coordinates) %>% max()
+
+#atm2=stb3[(n+5):(n+5+AN-1),]
+
+coordinates = Log_file[(num_of_line + 5):(num_of_line + 5 + num_of_atoms -1)]
 
 
-atm2=stb3[(n+5):(n+5+AN-1),]
+#atm_n2=atm2[] %>% stringr::str_split(" ") %>% type.convert() ##　文字列を分割して数値に変換
 
+coordinates_numerical_crude = coordinates[] %>% stringr::str_split(" ") %>% type.convert() 
 
-atm_n2=atm2[] %>% stringr::str_split(" ") %>% type.convert() ##　文字列を分割して数値に変換
+coordinates_numerical_crude
 
 #  atm_n2 = atm2[] %>% stringr::str_split(" ") 
 
 # atm_n2 = atm_n2[grep("^[9-9]{3}$",atm_n2)]
 
-A=matrix(0,26,6)
+A=matrix(0,num_of_atoms,6)
 
-f=A
+coordinates_numerical = A
 
-for (i in 1:26) {for (j in 1:6)
-{
-  f[i,j]=atm_n2[[i]][complete.cases(atm_n2[[i]])][j]
+
+for(i in 1:num_of_atoms){
+    for(j in 1:6){
+
+     coordinates_numerical[i,j]=coordinates_numerical_crude[[i]][complete.cases(coordinates_numerical_crude[[i]])][j]
+
+    }
 }
-  
-}
+
+coordinates_numerical
 
