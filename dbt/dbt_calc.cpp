c937a9e748ca5ef1ee6cc916c57e9f969e4af6bf
aki-ph-chem <77914655+aki-ph-chem@users.noreply.github.com>
2021-04-23 13:23:35 +0900

    中心差分で微分を実装（動くとは言っていない）

--- dbt/dbt_calc.cpp
+++ dbt/dbt_calc.cpp
@@ -33,9 +33,17 @@ string name_of_output_file = "angle_vs_rot_const_dbt.csv";
 int main(){
 
 data      Data;  // 座標データのインスタンス化
+
 rod_rot   RR;   //回転クラスのインスタンス化
+rod_rot   RR_1;
+rod_rot   RR_2;
+double d_angel;  //微分の刻み幅
+
 csv CC;         // csv loading
+
 I_tensor C;     //calc_gを継承したI_tensorのインスタンス化
+I_tensor C_1;
+I_tensor C_2;
 
 // loading data
 
@@ -90,11 +98,20 @@ g_sys = Eigen::MatrixXd::Zero(num_of_atoms,4);
 
 // 各種行列の宣言
 Eigen::Matrix3d  I;
+Eigen::Matrix3d  I_1;
+Eigen::Matrix3d  I_2;
+
 //ソルバの宣言
 Eigen::SelfAdjointEigenSolver<Eigen::Matrix3d> eigensolver;
 
 Eigen::Array3d  e_val;
+Eigen::Array3d  e_val_1;
+Eigen::Array3d  e_val_2;
+
 Eigen::Array3d rot_const;
+Eigen::Array3d rot_const_1;
+Eigen::Array3d rot_const_2;
+
 Eigen::Vector3d v;
 
 //データの初期化
@@ -114,6 +131,9 @@ C.cal_g_sys(g_point);
 Matrix_dx4 G_system_old = C.G_sys;
 Matrix_dx4 G_system_new = G_system_old;
 
+Matrix_dx4 G_system_1 = G_system_old;
+Matrix_dx4 G_system_2 = G_system_old;
+
 
 // set axis 
 int atom_1,atom_2;
@@ -127,7 +147,10 @@ RR.axis = G_system_old.row(atom_1).tail(3) - G_system_old.row(atom_2).tail(3);
 
 double norm_of_axis = RR.axis.norm();
 
-RR.axis = RR.axis/norm_of_axis ;
+RR.axis = RR.axis/norm_of_axis;
+RR_1.axis = RR.axis/norm_of_axis;
+RR_2.axis = RR.axis/norm_of_axis;
+
 
 
 
@@ -149,23 +172,39 @@ const int num_of_calc = (angle_end-angle_now)/step ;
 
 //ここからループ
 
+d_angel = 0.1; //刻み幅を0.1に
 
-for(int i=0;i<num_of_calc;i++){
+//for(int i=0;i<num_of_calc;i++){
 
 angle_now = angle_now + step;
 
 RR.set(angle_now);
+RR_1.set(angle_now + d_angel);
+RR_2.set(angle_now - d_angel);
+
 
 
 // rot S atom
 v = G_system_old.row(20).tail(3);
+
 G_system_new.row(20).tail(3) = RR.Rot*v;
+G_system_1.row(20).tail(3) = RR_1.Rot*v;
+G_system_2.row(20).tail(3) = RR_2.Rot*v;
 
 
 
 C.set_coordinates(G_system_new);
 C.calc_g_point();
 C.cal_g_sys(g_point);
+
+C_1.set_coordinates(G_system_new);
+C_1.calc_g_point();
+C_1.cal_g_sys(g_point);
+
+
+C_2.set_coordinates(G_system_new);
+C_2.calc_g_point();
+C_2.cal_g_sys(g_point);
 
 
 //慣性テンソルの計算
@@ -174,9 +213,18 @@ C.calc_I_tensor();
 I = C.I;
 
 
+C_1.calc_I_tensor();
+I_1 = C_1.I;
+
+
+C_2.calc_I_tensor();
+I_2 = C_2.I;
+
+
+
 //対角化計算
 eigensolver.compute(I);
- 
+
 if (eigensolver.info() != Eigen::Success) abort();
 
 e_val = eigensolver.eigenvalues();
@@ -184,11 +232,29 @@ e_val = eigensolver.eigenvalues();
 rot_const = 2.799275*pow(10,-26)/e_val ;
 
 
+eigensolver.compute(I_1);
+
+if (eigensolver.info() != Eigen::Success) abort();
+
+e_val_1 = eigensolver.eigenvalues();
+
+rot_const_1 = 2.799275*pow(10,-26)/e_val_1 ;
+
+
+eigensolver.compute(I_2);
+
+if (eigensolver.info() != Eigen::Success) abort();
+
+e_val_2 = eigensolver.eigenvalues();
+
+rot_const_2 = 2.799275*pow(10,-26)/e_val_2 ;
+
+
 ofs<<angle_now<<","<<rot_const(0)<<","<<rot_const(1)<<","<<rot_const(2)<<std::endl;
 
-}
+//}
 
-
+auto Result = ( rot_const_1 - rot_const_2 )/(2*d_angel);
 
 
 return 0;
